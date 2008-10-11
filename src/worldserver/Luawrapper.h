261ea37f8f0f718334977f34ac93fa1f0f8255e4
TheLuda <theluda@getmangos.com>
2008-10-11 14:12:53 +0200

    Removed generated files.

--- src/worldserver/Luawrapper.h
+++
@@ -1,9 +0,0 @@
-/*
-** Lua binding: worldsrv
-** Generated automatically by tolua++-1.0.5 on Sat May  7 03:50:05 2005.
-*/
-extern "C" {
-#include "lua.h"
-}
-/* Exported function */
-extern int tolua_worldsrv_open (lua_State* tolua_S);

074ccae10ba59e95d5bd46f34b211f3d6dad4753
TheLuda <theluda@getmangos.com>
2008-10-18 15:46:03 +0200

    Package autoconfiguration improved.

--- win/config.win.h
+++ win/config.win.h
@@ -174,7 +174,7 @@
 #define PACKAGE "mangos2"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "http://www.getmangos.com/"
+#define PACKAGE_BUGREPORT "http://mangos.lighthouseapp.com/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "mangos2"

7a93bf18b227edfc8508051607a077711bc4dd30
TheLuda <theluda@getmangos.com>
2008-10-24 23:44:29 +0200

    Fixed paths.

--- win/beforeworld.bat
+++ win/beforeworld.bat
@@ -1,3 +1,3 @@
-C:\Python25\python ../src/tools/gencode/gencode.py -t cxx -f ../src/worldsrv/Opcodes.cpp -w code ../src/worldsrv/Protocol.rules
-C:\Python25\python ../src/tools/gencode/gencode.py -t cxx -f ../src/worldsrv/Opcodes.h -w header ../src/worldsrv/Protocol.rules
-C:\Python25\python ../src/tools/gencode/gencode.py -t cxx -f ../src/worldsrv/Opcodes.inc -w parser ../src/worldsrv/Protocol.rules
+C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.cpp -w code ../../src/worldserver/Protocol.rules
+C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.h -w header ../../src/worldserver/Protocol.rules
+C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.inc -w parser ../../src/worldserver/Protocol.rules

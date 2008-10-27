e01334f8053db7c0132912efe4730278a6195b79
TheLuda <theluda@getmangos.com>
2008-10-27 12:46:39 +0100

    Generating opcode handlers from within the project files.

--- win/beforerealm.bat
+++
@@ -1,3 +0,0 @@
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/realmlist/RealmProto.cpp -w code ../../src/realmlist/RealmProto.rules
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/realmlist/RealmProto.h -w header ../../src/realmlist/RealmProto.rules
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/realmlist/RealmProto.inc -w parser ../../src/realmlist/RealmProto.rules
--- win/beforeworld.bat
+++
@@ -1,3 +0,0 @@
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.cpp -w code ../../src/worldserver/Protocol.rules
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.h -w header ../../src/worldserver/Protocol.rules
-C:\Python25\python ../../src/tools/gencode/gencode.py -t cxx -f ../../src/worldserver/Opcodes.inc -w parser ../../src/worldserver/Protocol.rules

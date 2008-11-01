066ca7626599bc9d5483d24bbe45b17acbe53c87
TheLuda <theluda@getmangos.com>
2008-11-01 10:40:46 +0100

    Switch to usin tolua++ 1.0.92 from dep/.

--- src/tools/tolua/bin/toluabind.h
+++
@@ -1,7 +0,0 @@
-/*
-** Lua binding: tolua
-** Generated automatically by tolua++-1.0.4 on Fri Apr  8 21:50:32 2005.
-*/
-
-/* Exported function */
-TOLUA_API int tolua_tolua_open (lua_State* tolua_S);
